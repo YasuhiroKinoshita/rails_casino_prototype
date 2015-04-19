b014a55a58fdfbf9577e4bc12b6c2387ff99926a
Kinoshita.Yasuhiro <WhoIsDissolvedGirl+github@gmail.com>
2015-04-19 12:12:47 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleCasino::Application.config.secret_key_base = '07bc24039c19702b1d8b34bbf2c62963d8e38bd3c3ab1d26cc855f5773a3970466f17690f115441e98177279764d362fc35eb633de25db218ed1170401614f7b'
