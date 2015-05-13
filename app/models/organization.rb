caa962d355602633143fd199a9e60895b87ce83a
Kinoshita.Yasuhiro <WhoIsDissolvedGirl+github@gmail.com>
2015-05-13 17:47:44 +0000

    apply bootstrap for organization view

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,8 +1,7 @@
 class Organization < ActiveRecord::Base
   has_many :games
   has_many :members
-  has_many :cashiers, through: :members
-  has_many :users, through: :organization
+  has_many :users, through: :members
   belongs_to :owner, class_name: 'User', foreign_key: :owner_id
 
   validates :name, length: { maximum: 50 }, presence: true
