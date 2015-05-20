c9c65177acfdaa7ddb1df72fa0ba3f12e05f5788
Kinoshita.Yasuhiro <WhoIsDissolvedGirl+github@gmail.com>
2015-05-20 21:02:58 +0900

    add slack auth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, Settings.twitter.consumer_key, Settings.twitter.consumer_secret
   provider :facebook, Settings.facebook.key, Settings.facebook.secret
+  provider :slack, Settings.slack.client_id, Settings.slack.client_secret, scope: 'read'
 end
